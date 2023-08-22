3ad8f6b9d3336d21270545a714b9b5d670116558
yuvrajaudacious <audaciousyuvraj227@gmail.com>
2023-08-22 19:48:06 +0530

    first commit

---
+++ src/Header/Head.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Styles from "./style.module.css";
+const Head = () => {
+  return (
+    <div className={Styles.box}>
+    <div className={Styles.box}>
+    <p>Mai Ek I Phone Hoon</p>
+    </div>
+    <div className={Styles.box1}>
+    <p>Mai Ek Tab Hoon</p>
+    </div>
+    <div className={Styles.box2}>
+    <p>Mai Ek Computer Hoon</p>
+    </div>
+    <div className={Styles.box3}>
+    <p>Mai Ek Webscreen Hoon</p>
+    </div>
+    </div>
+  );
+};
+
+export default Head;
